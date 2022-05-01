ecdd4d00d03908a50c339ea713c0935147920e53
Nelson Rosales <nelson.rosales83@gmail.com>
2022-05-01 20:50:21 +0200

    Single post styles added

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -14,7 +14,27 @@ const PostPage = ({
   return (
     <Layout title={title}>
       <Link href="/blog">Go Back</Link>
-      <div className="w-full px-10 py-6 bg-white rounded-lg shadow-md mt-6"></div>
+      <div className="w-full px-10 py-6 bg-white rounded-lg shadow-md mt-6">
+        <div className="flex justify-between items-center mt-4">
+          <h1 className="text-5xl mb-7">{title}</h1>
+          <CategoryLabel>{category}</CategoryLabel>
+        </div>
+        <img src={cover_image} alt="" className="w-full rounded" />
+        <div className="flex justify-between items-center bg-gray-100 p-2 my-8">
+          <div className="flex items-center">
+            <img
+              src={author_image}
+              alt="author image"
+              className="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block"
+            ></img>
+            <h4>{author}</h4>
+          </div>
+          <div className="mr-4">{date}</div>
+        </div>
+        <div className="blog-text mt-2">
+          <div dangerouslySetInnerHTML={{ __html: marked(content) }}></div>
+        </div>
+      </div>
     </Layout>
   );
 };
